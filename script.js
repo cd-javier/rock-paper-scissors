12edc356cb631cf6ff36b281a78c68fb54b9f082
Javier Quiroga <contact@javierquiroga.com>
2024-11-18 16:44:38 +0000

    Make playable on the console using UI

--- script.js
+++ script.js
@@ -4,25 +4,6 @@ function getComputerChoice() {
   return gameChoices[Math.floor(Math.random() * 3)];
 }
 
-function getHumanChoice() {
-  let humanChoice = prompt('Choose between "Rock", "Paper", or "Scissors', ""); // The player is asked to choose
-
-  if (
-    // Check that the user has entered a valid answer
-    (humanChoice.toLowerCase() !== "rock" &&
-      humanChoice.toLowerCase() !== "paper" &&
-      humanChoice.toLowerCase() !== "scissors") ||
-    humanChoice === null
-  ) {
-    alert("Hmm... that wasn't an option. Try again.");
-    humanChoice = getHumanChoice(); // If it isn't valid, we restart the function
-  }
-  return humanChoice.toLowerCase(); // We turn the answer to lowercase to not be case-sensitive
-}
-
-let computerScore = 0;
-let humanScore = 0;
-
 function getWinPhrase() {
   // Create an array of different phrases to cheer the user
   const phrases = [
@@ -66,48 +47,38 @@ function getTiePhrase() {
   return phrases[Math.floor(Math.random() * 8)];
 }
 
+let computerScore = 0;
+let humanScore = 0;
+
 function playRound(humanChoice, computerChoice) {
+  console.log("Computer chose: " + computerChoice);
   if (
     (humanChoice === "rock" && computerChoice === "scissors") || // Basic game play
     (humanChoice === "paper" && computerChoice === "rock") ||
     (humanChoice === "scissors" && computerChoice === "paper")
   ) {
     humanScore++; // Score is updated
-    alert(`${getWinPhrase()}
-        Your score: ${humanScore}
-        Computer's score: ${computerScore}`);
+    console.log("You won");
   } else if (humanChoice === computerChoice) {
-    alert(`${getTiePhrase()}
-        Your score: ${humanScore}
-        Computer's score: ${computerScore}`);
+    console.log("You tied");
   } else {
     computerScore++;
-    alert(`${getLosePhrase()}
-        Your score: ${humanScore}
-        Computer's score: ${computerScore}`);
+    console.log("You lost");
   }
+  console.log("Your score: " + humanScore + " | Computer score: " + computerScore);
 }
 
-function playFiveRounds() {
-  for (i = 1; i <= 5; i++) {
-    // Play 5 rounds
-    if (i === 1) {
-      alert("Let's play some Rock, Paper, Scissors!"); // Initial message
-    }
+const gameChoice = document.querySelectorAll(".game-choice");
+let humanChoice;
 
-    alert("ROUND " + i); // Announce the round
+gameChoice.forEach((button) => {
+  button.addEventListener("click", () => {
+    humanChoice = button.textContent.toLowerCase();
+    console.log("You chose: " + humanChoice);
+  });
+});
 
-    playRound(getHumanChoice(), getComputerChoice()); // Play game with getHumanChoice() and getComputerChoice() as arguments
-  }
-
-  if (humanScore > computerScore) {
-    // Decide the final winner looking at the scores
-    alert("Congratulations, you win!");
-  } else if (humanScore < computerScore) {
-    alert("You lose, try again!");
-  } else {
-    alert("You tied!");
-  }
-}
-
-playFiveRounds();
+const playButton = document.querySelector("#play-button");
+playButton.addEventListener("click", () => {
+  playRound(humanChoice, getComputerChoice());
+});
